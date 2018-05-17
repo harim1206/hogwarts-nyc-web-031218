a5467a564f150d8b7b4673b2e46dc08a706c9fea
Dwire <gjdwyer8@gmail.com>
2018-05-17 15:17:01 -0400

    MVP

--- src/components/HogTile.js
+++ src/components/HogTile.js
@@ -3,16 +3,33 @@ import React, { Component } from 'react'
 import HogDetails from './HogDetails.js'
 
 class HogTile extends Component{
+  state = {
+    clicked: false
+  }
+
+  handleClick = () => {
+    const toggle = !this.state.clicked
+    this.setState({clicked: toggle})
+  }
 
   render(){
-    console.log(this.props);
-    return (
-      <div className="ui eight wide column">
-        <p> {this.props.name} </p>
-        <img src={this.props.url} alt="pig pic" />
-        <HogDetails specialty={this.props.specialty} greased={this.props.greased} weight={this.props.weight} medal={this.props.medal}/>
-      </div>
-    )
+    if(this.state.clicked === true) {
+      return (
+        <div className="ui eight wide column tiles" onClick={this.handleClick}>
+          <p> {this.props.name} </p>
+          <img src={this.props.url} alt="pig pic" />
+          <HogDetails specialty={this.props.specialty} greased={this.props.greased} weight={this.props.weight} medal={this.props.medal}/>
+        </div>
+      )
+    }else {
+      return(
+        <div className="ui eight wide column tiles" onClick={this.handleClick}>
+          <p> {this.props.name} </p>
+          <img src={this.props.url} alt="pig pic" />
+        </div>
+      )
+    }
+
   }
 }
 
--- src/components/HogsMain.js
+++ src/components/HogsMain.js
@@ -3,18 +3,50 @@ import HogTile from './HogTile.js'
 
 
 class HogsMain extends Component{
+  state = {
+    sortBy: "default"
+  }
+
+  handleName = () => {
+    console.log("Name Sort");
+    this.setState({sortBy: "name"})
+  }
+  handleWeight = () => {
+    console.log("Weight Sort");
+    this.setState({sortBy: "weight"})
+  }
 
   render(){
     // debugger
-    const hogsTiles = this.props.hogs.map((hog) => {
+    let orderedHogs = null
+
+    if (this.state.sortBy === "default") {
+      orderedHogs = this.props.hogs
+    }else if (this.state.sortBy === "name") {
+      orderedHogs = this.props.hogs.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      })
+    }else if (this.state.sortBy === "weight"){
+      orderedHogs = this.props.hogs.sort((a, b) => {
+        return a['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] - b['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']
+      })
+    }
+
+
+    const hogsTiles = orderedHogs.map((hog) => {
       return <HogTile name={hog.name} url={hog.imgUrl}  specialty={hog.specialty} greased={hog.greased} weight={hog[this.props.weightKey]}
       medal={hog[this.props.medalKey]}/>
     })
 
     return (
-
-      <div className="ui grid container">
-        {hogsTiles}
+      <div>
+        <div className="button-container">
+          <button onClick={this.handleName}>Sort by Name</button>
+          <button onClick={this.handleWeight}>Sort by Weight</button>
+        </div>
+        <div className="ui grid container">
+          {hogsTiles}
+        </div>
       </div>
 
 
